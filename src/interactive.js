09fb741901e23d49c48dffe4334f0bc5d76a41b2
David Waring <dev@davidwaring.net>
2017-11-13 18:44:51 -0500

    Interactive: prompt improvements
    redisplay prompt after blank line
    unrecognized commands

--- src/interactive.js
+++ src/interactive.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const log = require('./utils/log.js');
+
+
 /**
  * Display the Interactive Mode Prompt
  */
@@ -9,24 +12,45 @@ function prompt() {
   // Display the Prompt
   global._rl.question("> ", function(line) {
 
+    // Parse the command from the entered line
     let params = line.trim().split(' ');
-    let cmd = params[0];
-    params.unshift(process.argv[0], process.argv[1]);
+    let cmd = params[0].toLowerCase();
+    params.unshift(process.argv[0], process.argv[1])
+
+    // Get possible commands
+    let commands = [];
+    for ( let i = 0; i < global._program.commands.length; i++ ) {
+      commands.push(global._program.commands[i].name());
+    }
 
     // Exit
-    if ( cmd.toLowerCase() === 'quit' || cmd.toLowerCase() === 'exit' ) {
+    if ( cmd === 'quit' || cmd === 'exit' ) {
       global._rl.close();
       process.exit(0);
     }
 
     // Help
-    if ( cmd.toLowerCase() === 'help' ) {
+    else if ( cmd === 'help' ) {
       global._program.outputHelp();
       prompt();
     }
 
     // Parse the line command with commander
-    global._program.parse(params);
+    else if ( commands.indexOf(cmd) > -1 ) {
+      global._program.parse(params);
+    }
+
+    // Nothing entered
+    else if ( cmd === '' ) {
+      prompt();
+    }
+
+    // Unknown command
+    else {
+      log.spinner.error("Unknown Command");
+      log.style("Commands: help," + commands.join(',') + ",quit", "dim", true);
+      prompt();
+    }
 
   });
 
