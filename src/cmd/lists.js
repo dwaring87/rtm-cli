b678d954f02e2c23ce77b12dbdf58c37be831c80
David Waring <dev@davidwaring.net>
2017-11-14 12:59:16 -0500

    Command: lists
    Display a list of all RTM Lists

---
+++ src/cmd/lists.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const log = require('../utils/log.js');
+const config = require('../utils/config.js');
+const styles = config.get().styles;
+
+
+/**
+ * This command removes any saved User information from the
+ * config files.
+ */
+function action(args, env, callback) {
+  config.user(function(user) {
+    log.spinner.start("Getting Lists...");
+
+    user.lists.get(function(err, lists) {
+      if ( err ) {
+        log.spinner.error("Could not get lists (" + err.msg + ")");
+        return callback();
+      }
+      log.spinner.stop();
+
+      // Display List Information
+      for ( let i = 0; i < lists.length; i++ ) {
+        let list = lists[i];
+
+        // List ID, if requested
+        if ( args.id ) {
+          log.style(list.id + " ", styles.index);
+        }
+
+        // List Name
+        log.style(list.name, styles.list);
+
+        // List Locked
+        if ( list.locked ) {
+          log.style(" locked", "dim");
+        }
+
+        // List Smart Filter
+        if ( list.smart ) {
+          log.style(" " + list.filter, "blue");
+        }
+
+        log();
+      }
+
+      return callback();
+    });
+  });
+}
+
+
+module.exports = {
+  command: 'lists',
+  alias: 'l',
+  description: 'Display all lists',
+  options: [
+    {
+      option: "--id",
+      description: "Display List ID Numbers"
+    }
+  ],
+  action: action
+};
