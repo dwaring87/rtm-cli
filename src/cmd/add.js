7e5978cb3f70fb96e27d19f3521e410dd1d7793c
David Waring <dev@davidwaring.net>
2017-11-13 21:41:15 -0500

    Add: remove alias ‘a’
    too difficult to support aliases

--- src/cmd/add.js
+++ src/cmd/add.js
@@ -135,7 +135,6 @@ function _taskAdded(err, index) {
 
 module.exports = {
   command: 'add [task...]',
-  alias: 'a',
   description: 'Add a new Task [task name due date p:priority l:list t:tag]',
   action: action
 };
