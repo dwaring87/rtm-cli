3f8f486159283b1913605abbbe142b36deb4cae9
David Waring <dev@davidwaring.net>
2017-11-15 18:30:41 -0500

    Pri: clean command syntax

--- src/cmd/pri.js
+++ src/cmd/pri.js
@@ -1,132 +1,75 @@
 'use strict';
 
+const prompt = require('../utils/prompt.js');
 const log = require('../utils/log.js');
 const config = require('../utils/config.js');
-
-let USER = undefined;
-let TASKS = undefined;
-let TASKS_PROCESSED = undefined;
-let CALLBACK = undefined;
+const finish = require('../utils/finish.js');
 
 
 /**
- * This command changes the priority of  1 or more tasks
+ * This command updates the priority of 1 or more Tasks
+ * @param args index priority
+ * @param env
  */
-function action(args, env, callback) {
+function action(args, env) {
+
+  // Process the given arguments
+  if ( args.length === 2 ) {
+    _process(args[0], args[1]);
+  }
+
+  // Prompt the User for the arguments
+  else {
+    prompt("Task:", "Priority:", _promptFinished);
+  }
+
+}
+
+
+/**
+ * Process the returned answers
+ * @private
+ */
+function _promptFinished(answers) {
+  for ( let i = 0; i < answers.length; i++ ) {
+    let answer = answers[i];
+    _process(answer[0], answer[1], i+1, answers.length);
+  }
+}
+
+
+/**
+ * Process the request
+ * @private
+ */
+function _process(index, priority, count=1, max=1) {
+  log.spinner.start("Setting Task Priority...");
   config.user(function(user) {
+    let _index = parseInt(index.trim());
+    let _priority = parseInt(priority.trim());
 
-    // Reset properties
-    USER = user;
-    TASKS = [];
-    TASKS_PROCESSED = 0;
-    CALLBACK = callback;
-
-    // No index given, prompt for task indices
-    if ( args.length !== 2 || args[0] === undefined || args[1] === undefined ) {
-      _prompt();
-    }
-
-    // Task index given
-    else {
-      TASKS.push({
-        task: args[0],
-        priority: args[1]
-      });
-      _processTasks();
-    }
-
-  });
-}
-
-
-/**
- * Prompt for task indices
- * @private
- */
-function _prompt() {
-
-  // Prompt for Task Index
-  global._rl.question('Task: ', function(task) {
-    if ( task === '' ) {
-      _processTasks();
-    }
-    else {
-
-      // Prompt for Task Priority
-      global._rl.question('Priority: ', function(priority) {
-        if ( priority === '' ) {
-          _processTasks();
-        }
-        else {
-          TASKS.push({
-            task: parseInt(task.trim()),
-            priority: parseInt(priority.trim())
-          });
-          _prompt();
-        }
-
-      });
-    }
-  });
-}
-
-
-/**
- * Process the Tasks
- * @private
- */
-function _processTasks() {
-  if ( TASKS.length === 0 ) {
-    return CALLBACK();
-  }
-  else {
-    log.spinner.start("Setting Task Priorities...");
-    let timeout = 0;
-    for ( let i = 0; i < TASKS.length; i++ ) {
-      setTimeout(function() {
-        _processTask(i)
-      }, timeout);
-      timeout = timeout + 1000;
-    }
-  }
-}
-
-/**
- * Process the Task specified by the index
- * @param index
- * @private
- */
-function _processTask(index) {
-  let task = parseInt(TASKS[index].task);
-  let priority = parseInt(TASKS[index].priority);
-  USER.tasks.priority(task, priority, function(err) {
-    _taskProcessed(err, index);
+    user.tasks.priority(_index, _priority, function(err) {
+      if ( err ) {
+        log.spinner.error("Could not update Task #" + _index + " (" + err.msg + ")");
+      }
+      _processFinished(count, max);
+    });
   });
 }
 
 /**
- * Callback function for when a task is processed
- * @param err RTMError encountered while processing
- * @param index The index of the task processed
+ * Request Callback
  * @private
  */
-function _taskProcessed(err, index) {
-  TASKS_PROCESSED++;
-
-  // Display Add Status
-  if ( err ) {
-    log.spinner.error("Could not change priority of task " + TASKS[index].task + " (" + err.msg + ")");
-  }
-  else {
-    log.spinner.start("Tasks Updated [" + TASKS_PROCESSED + "/" + TASKS.length + "]...");
-  }
-
-  // Finish when all tasks have been added
-  if ( TASKS_PROCESSED === TASKS.length ) {
+function _processFinished(count, max) {
+  log.spinner.start("Setting Task Priority [" + count + "/" + max + "]...");
+  if ( count === max ) {
     log.spinner.success("Task(s) Updated");
-    return CALLBACK();
+    return finish();
   }
 }
+
+
 
 
 module.exports = {
