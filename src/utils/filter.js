71081c26381ced4014760dd3ad9a07b8d9d470dd
David Waring <dev@davidwaring.net>
2017-11-29 15:03:15 -0500

    Filter: improve building filter string
    put built-in completed filter first
    better enclose filters in ()

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -8,20 +8,27 @@ const config = require('../utils/config.js');
  * @param filter
  */
 function filter(filter) {
+
+  // Generate Completed Filter
+  let compFilter = '';
   let comp = config.get().completed;
   if ( comp === false ) {
-    if ( filter !== '' ) {
-      filter += " AND ";
-    }
-    filter += 'status:incomplete';
+    compFilter = 'status:incomplete';
   }
   else if ( comp !== true && comp > 0 ) {
-    if ( filter !== '' ) {
-      filter += " AND ";
-    }
-    filter += '(status:incomplete OR completedWithin:"' + comp + ' days")';
+    compFilter = 'status:incomplete OR completedWithin:"' + comp + ' days"';
   }
-  return filter;
+
+  // Add user filter
+  let rtn = '';
+  if ( filter.trim() !== '' ) {
+    rtn = '(' + compFilter + ') AND (' + filter + ')';
+  }
+  else {
+    rtn = compFilter;
+  }
+
+  return rtn;
 }
 
 module.exports = filter;
