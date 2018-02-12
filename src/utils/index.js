595e57b01a3afa1fe5d3427da6674f36349a666d
David Waring <dev@davidwaring.net>
2018-02-12 17:03:45 -0500

    Export usefult utils

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  config: require('./config.js'),
+  filter: require('./filter.js'),
+  log: require('./log.js'),
+  login: require('./login.js'),
+  prompt: require('./prompt.js')
+};
