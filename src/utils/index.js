91ddd5928456d324ed6098ed65bb83f365b5abb3
David Waring <dev@davidwaring.net>
2018-02-12 20:01:56 -0500

    Add finish() to utils export

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   config: require('./config.js'),
   filter: require('./filter.js'),
+  finish: require('./finish.js'),
   log: require('./log.js'),
   login: require('./login.js'),
   prompt: require('./prompt.js')
